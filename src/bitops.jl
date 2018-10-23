4848b117d89de0ea3e64b363fdf6e3a9b88c0ea2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-23 01:40:49 -0400

    Update bitops.jl

--- src/bitops.jl
+++ src/bitops.jl
@@ -1,5 +1,4 @@
 bitsizeof(::Type{T}) where {T} = sizeof(T) * 8
-bitsizeof(x::T) where {T} = bitsizeof(T)
 
 onebits(::Type{T}, bitspan::Int, bitshift::Int) where {T<:Unsigned} =
     ~(~zero(T) << bitspan) << bitshift
