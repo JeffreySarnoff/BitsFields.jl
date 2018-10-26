70d766b01229b04f9f122abdf4e1ee74b546caf5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 08:24:13 -0400

    Update bitfield.jl

--- src/bitfield.jl
+++ src/bitfield.jl
@@ -64,3 +64,9 @@ function validate(::Type{U}, bitspan::Integer, bitshift::Integer) where {U<:UBit
     end
     return nothing
 end
+
+
+bitsizeof(::Type{T}) where {T} = sizeof(T) * 8
+
+onebits(::Type{T}, bitspan::I, bitshift::I) where {T<:UBits, I<:Integer} =
+    ~(~zero(T) << bitspan) << bitshift
