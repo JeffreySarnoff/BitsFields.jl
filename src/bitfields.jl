4414ed62f2bf508775eb2941824efd238cc4407f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 06:34:43 -0400

    Update bitfields.jl

--- src/bitfields.jl
+++ src/bitfields.jl
@@ -1,4 +1,4 @@
-struct BitFields{N,U}    # N UIntBits{U} fields
+struct BitFields{N,U}    # N BitField{U} fields
    bitfields::NTuple{N,BitField{U}}
 
    function BitFields(bitfields::Vararg{BitField{U}, N}) where {N,U}
