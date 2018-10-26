3faa6cde2f06bba551d58bf07ef7b411e601b1d5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-26 06:15:36 -0400

    Update bitfield.jl

--- src/bitfield.jl
+++ src/bitfield.jl
@@ -1,40 +1,40 @@
 const BitUInt = Int16
 
-struct UIntBits{U}
+struct BitField{U}
     nbits::BitUInt
     shift::BitUInt
     maskof1s::U
     maskof0s::U
 end
 
-function UIntBits(::Type{U}, bitspan::Int, bitshift::Int) where {U<:BitUInt}
+function BitField(::Type{U}, bitspan::Int, bitshift::Int) where {U<:BitUInt}
     span  = BitUInt(bitspan)
     shift = BitUInt(bitshift)
     validate(U, span, shift)
 
     maskof1s = onebits(U, span, shift)
     maskof0s = ~maskof1s
-    return UIntBits{U}(span, shift, maskof1s, maskof0s)
+    return BitField{U}(span, shift, maskof1s, maskof0s)
 end
 
 
-function isolate(source::U, bitfield::UIntBits{U}) where {U<:BitUInt}
+function isolate(source::U, bitfield::BitField{U}) where {U<:BitUInt}
     return source & bitfield.maskof1s
 end
 
-function Base.get(source::U, bitfield::UIntBits{U}) where {U<:BitUInt}
+function Base.get(source::U, bitfield::BitField{U}) where {U<:BitUInt}
     return isolate(source, bitfield) >> bitfield.shift
 end
 
-function filter(source::U, bitfield::UIntBits{U}) where {U<:BitUInt}
+function filter(source::U, bitfield::BitField{U}) where {U<:BitUInt}
     return source & bitfield.maskof0s
 end
 
-function put(value::U, bitfield::UIntBits{U}) where {U<:BitUInt}
+function put(value::U, bitfield::BitField{U}) where {U<:BitUInt}
     value << bitfield.shift
 end
 
-function set(target::U, value::U, bitfield::UIntBits{U})  where {U<:BitUInt}
+function set(target::U, value::U, bitfield::BitField{U})  where {U<:BitUInt}
     filter(target, bitfield) | put(value, bitfield)
 end
 
