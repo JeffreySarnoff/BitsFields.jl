32e3ba8c73a9fb785411d0ee800dafb971e0a876
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-20 10:32:53 -0500

    MaybeSymbol

--- src/BitsFields.jl
+++ src/BitsFields.jl
@@ -2,7 +2,19 @@ module BitsFields
 
 export BitField, BitFields, set!
 
-const UBits = Union{UInt128,UInt64,UInt32,UInt16,UInt8}
+"""
+    MaybeSymbol
+
+allow names, rather than require them
+"""
+const MaybeSymbol = Union{Nothing, Symbol}
+
+"""
+    UBits
+
+unsigned types available to hold multiple bitfields
+"""
+const UBits = Union{UInt128, UInt64, UInt32, UInt16, UInt8}
 
 include("bitfield.jl")
 include("bitfields.jl")
