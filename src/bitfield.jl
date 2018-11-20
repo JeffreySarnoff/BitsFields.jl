d4db85ba08e743445a2e63d9471d13c443751f85
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-20 10:03:23 -0500

    Update bitfield.jl

--- src/bitfield.jl
+++ src/bitfield.jl
@@ -13,18 +13,25 @@ Base.eltype(x::BitField{U}) where {U} = U
 
 symbol(x::BitField{U}) where {U<:UBits} = x.symbol
 
-function BitField(::Type{U}, bitspan::Int, bitshift::Int) where {U<:UBits}
+function BitField(::Type{U}, bitspan::Int, bitshift::Int, symbol::Symbol) where {U<:UBits}
     span  = BitCount(bitspan)
     shift = BitCount(bitshift)
     validate(U, span, shift)
 
     maskof1s = onebits(U, span, shift)
     maskof0s = ~maskof1s
-    return BitField{U}(span, shift, maskof1s, maskof0s)
+    return BitField{U}(span, shift, maskof1s, maskof0s, symbol)
 end
 
-BitField(bitspan::Int, bitshift::Int) = BitField(UInt64, bitspan, bitshift)
-        
+BitField(::Type{U}, symbol::Symbol, bitspan::Int, bitshift::Int) where {U<:UBits} =
+    BitField(U, bitspan, bitshift, symbol)
+
+BitField(bitspan::Int, bitshift::Int, symbol::Symbol) = BitField(UInt64, bitspan, bitshift, symbol)
+
+BitField(symbol::Symbol, bitspan::Int, bitshift::Int) = BitField(UInt64, bitspan, bitshift, symbol)
+
+
+
 @inline function isolate(bitfield::BitField{U}, source::U) where {U<:UBits}
     return source & bitfield.maskof1s
 end
