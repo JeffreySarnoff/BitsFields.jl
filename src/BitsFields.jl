6fcce52209bb5f33888d1ff3f403d28f95ab4890
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-20 19:41:49 -0500

    Update BitsFields.jl

--- src/BitsFields.jl
+++ src/BitsFields.jl
@@ -16,6 +16,14 @@ unsigned types available to hold multiple bitfields
 """
 const UBits = Union{UInt128, UInt64, UInt32, UInt16, UInt8}
 
+"""
+    bitsof
+
+as sizeof is with bytes, bitsof is with bits
+"""
+bitsof(::Type{T}) where {T} = sizeof(T) * 8
+
+
 include("bitfield.jl")
 include("bitfields.jl")
 
