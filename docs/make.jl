01c05009e65fc4d32093f373b4962cfed5a14b46
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-20 19:56:18 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,25 @@
+using Documenter, BitsFields
 
+makedocs(
+    modules = [BitsFields],
+    format = :html,
+    sitename = "BitsFields",
+    pages    = Any[
+        "Introduction"             => "index.md",
+        "The BitField"             => "thebitfield.md",
+        "The BitField, Named"      => "thebitfieldnamed.md",
+        "MultiFields"              => "multifields.md",
+        "MultiFields, Named"       => "multifieldsnamed.md",
+        "References"               => "references.md",
+        "Index"                    => "index.md",
+        ]
+    )
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/BitsFields.jl.git",
+    target = "build",
+    julia  = "1.0",
+    osname = "linux",
+    deps = nothing,
+    make = nothing
+)
