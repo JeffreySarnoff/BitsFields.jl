75c001afc9eb684469b70f99622094fc1ee8a77c
autodocs <autodocs>
2018-11-26 23:20:21 +0000

    build based on a4cbcf1

--- dev/search_index.js
+++ dev/search_index.js
@@ -32,4 +32,140 @@ var documenterSearchIndex = {"docs": [
     "text": "abcd"
 },
 
+{
+    "location": "thebitfieldnamed/#",
+    "page": "The BitField, Named",
+    "title": "The BitField, Named",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "thebitfieldnamed/#The-BitField,-Named-1",
+    "page": "The BitField, Named",
+    "title": "The BitField, Named",
+    "category": "section",
+    "text": "abcd"
+},
+
+{
+    "location": "multifields/#",
+    "page": "MultiFields",
+    "title": "MultiFields",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "multifields/#MultiFields-1",
+    "page": "MultiFields",
+    "title": "MultiFields",
+    "category": "section",
+    "text": "abcd"
+},
+
+{
+    "location": "multifieldsnamed/#",
+    "page": "MultiFields, Named",
+    "title": "MultiFields, Named",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "multifieldsnamed/#MultiFields,-Named-1",
+    "page": "MultiFields, Named",
+    "title": "MultiFields, Named",
+    "category": "section",
+    "text": "abcd"
+},
+
+{
+    "location": "workeduse/#",
+    "page": "Worked Use",
+    "title": "Worked Use",
+    "category": "page",
+    "text": "(Image: IEEE754 Floating Point Standard)"
+},
+
+{
+    "location": "ieeestandard754-2008/#",
+    "page": "IEEE Standard 754-2008",
+    "title": "IEEE Standard 754-2008",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "ieeestandard754-2008/#IEEE-Standard-754-2008,-and-the-754-2018-revision-1",
+    "page": "IEEE Standard 754-2008",
+    "title": "IEEE Standard 754-2008, and the 754-2018 revision",
+    "category": "section",
+    "text": "(Image: Standard Float64 bitfields)"
+},
+
+{
+    "location": "ieeestandard754-2008/#the-representation-1",
+    "page": "IEEE Standard 754-2008",
+    "title": "the representation",
+    "category": "section",
+    "text": "2.1.26 floating-point representation: An unencoded member of a floating-point format, representing afinite number, a signed infinity, a quiet NaN, or a signaling NaN. A representation of a finite number has three components: a sign, an exponent, and a significand; its numerical value is the signed product of its significand and its radix raised to the power of its exponent.(Image: Standard Floating Point Specifications)This view of the significand as an integer c, with its corresponding exponent q, describes exactly the same set of zero and non-zero floating-point numbers as the view in scientific form. [&sect;3.3]"
+},
+
+{
+    "location": "ieeestandard754-2008/#the-sign-1",
+    "page": "IEEE Standard 754-2008",
+    "title": "the sign",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "ieeestandard754-2008/#the-significand-1",
+    "page": "IEEE Standard 754-2008",
+    "title": "the significand",
+    "category": "section",
+    "text": "2.1.49 significand: A component of a finite floating-point number containing its significant digits. Thesignificand can be thought of as an integer, a fraction, or some other fixed-point form, by choosing an appropriate exponent offset. A decimal or subnormal binary significand can also contain leading zeros."
+},
+
+{
+    "location": "ieeestandard754-2008/#the-exponent-1",
+    "page": "IEEE Standard 754-2008",
+    "title": "the exponent",
+    "category": "section",
+    "text": "2.1.19 exponent: The component of a finite floating-point representation that signifies the integer power towhich the radix is raised in determining the value of that floating-point representation. The exponent e is used when the significand is regarded as an integer digit and fraction field, and the exponent q is used when the significand is regarded as an integer; e = q + p − 1 where p is the precision of the format in digits."
+},
+
+{
+    "location": "references/#",
+    "page": "References",
+    "title": "References",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "references/#References-1",
+    "page": "References",
+    "title": "References",
+    "category": "section",
+    "text": "abcd"
+},
+
+{
+    "location": "documentindex/#",
+    "page": "Index",
+    "title": "Index",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "documentindex/#Concepts-and-Functions-1",
+    "page": "Index",
+    "title": "Concepts and Functions",
+    "category": "section",
+    "text": "defg"
+},
+
 ]}
