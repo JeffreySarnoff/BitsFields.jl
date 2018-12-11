be746d1f23a7d64c0711982a2996a3763f986d35
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-11 08:32:47 -0500

    Update byref.jl

--- src/byref.jl
+++ src/byref.jl
@@ -1,31 +1,34 @@
-struct ByRef{T}
+struct ByRef{S,T}
    ref::Ref{T}
    
-   function ByRef(x::Ref{T}) where {T}
-       return new{T}(x)
+   function ByRef(::Type{S}, x::Ref{T}) where {S,T}
+       return new{S,T}(x)
    end
 end
 
-@inline function ByRef(::Type{T}) where {T}
-    return ByRef(Ref(zero(T)))
+@inline function ByRef(::Type{S}, ::Type{T}) where {S,T}
+    return ByRef(S, Ref(zero(T)))
 end
 
-@inline function ByRef(x::T) where {T}
-    return ByRef(Ref(x))
+@inline function ByRef(::Type{S}, x::T) where {S,T}
+    return ByRef(S, Ref(x))
 end
 
-@inline ref(x::ByRef{T}) where {T} = x.ref
-@inline value(x::ByRef{T}) where {T} = x.ref[]
+@inline sourcetype(x::ByRef{S,T}) where {S,T} = S
+@inline carriertype(x::ByRef{S,T}) where {S,T} = T
 
-Base.show(io::IO, x::ByRef{T}) where {T} = show(io, x.ref[])
+@inline ref(x::ByRef{S,T}) where {S,T} = x.ref
+@inline value(x::ByRef{S,T}) where {S,T} = x.ref[]
 
-function set!(x::ByRef{T}, value::T) where {T} 
+Base.show(io::IO, x::ByRef{S,T}) where {S,T} = show(io, reinterpret(S,x.ref[]))
+
+function set!(x::ByRef{S,T}, value::T) where {S,T} 
     x.ref[] = value
     return x
 end
 
-Base.get(x::ByRef{T}) where {T} = x.ref[]
+Base.get(x::ByRef{S,T}) where {S,T} = x.ref[]
 
-Base.zero(::Type{ByRef{T}}) where {T} = ByRef(T)
+Base.zero(::Type{ByRef{S,T}}) where {S,T} = ByRef(S,T)
 
-Base.zero(x::ByRef{T}) where {T} = set!(x, zero(T))
+Base.zero(x::ByRef{S,T}) where {S,T} = set!(x, zero(T))
