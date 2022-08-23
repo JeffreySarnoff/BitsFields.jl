048d4c1276e4f8b1a16cb339b45e488b7588aa30
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-23 17:57:15 -0400

    Update BitsFields.jl

--- src/BitsFields.jl
+++ src/BitsFields.jl
@@ -2,5 +2,20 @@ module BitsFields
 
 export BitField, BitFields
 
+const CountType = UInt16
 
+struct BitField{T<:Base.BitInteger}
+    underlying::T     # underlying type
+    field0s::T        # filters field in place
+    field1s::T        # masks   field in place
+    mask0s::T         # filters field in lsbs
+    mask1s::T         # masks   field in lsbs
+    nbits::CountType  # bitwdith of field
+    shift::CountType  # shift from lsbs to field
 end # BitsFields
+
+struct BitFields{N,T}
+    fields::NTuple{N,BitField{T}}
+end
+
+end  # BitsFields
