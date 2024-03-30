43990492ec515f3ed68c89d0145e4c74f016f70f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-03-29 21:45:25 -0400

    Update BitsFields.jl

--- src/BitsFields.jl
+++ src/BitsFields.jl
@@ -30,7 +30,6 @@ include("byref.jl")
 include("bitfield.jl")
 include("bitfields.jl")
 
-
 Base.zero(bitfield::BitField{U}) where {U<:UBits} = zero(U)
 Base.zero(bitfields::BitFields{N,U}) where {N, U<:UBits} = Ref(zero(eltype(bitfields[1])))
 Base.zero(x::NamedTuple{Names, NTuple{N,BitField}}) where {Names, N} = Ref(zero(eltype(x[1])))
